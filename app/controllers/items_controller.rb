4d81b7eac687d090e1705ea4e4a39ab07b09c075
Beau <beaulazear@gmail.com>
2023-06-19 11:05:02 -0400

    commit tree bab22465e064c99491ee98e5d8f280396f80de87

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      userItems = user.items
+    else
+      userItems = Item.all
+    end
+    render json: userItems, include: :user
+  end
+
+  def show
+    item = Item.find_by!(id: params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find_by!(id: params[:user_id])
+    newItem = user.items.create(items_params)
+    render json: newItem, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :description, :price)
   end
 
 end
